140d657eaefdc074a8eb6d5259323b843cab7fba
Fernando Moreira <fernando@parteincerta.com>
2024-11-21 00:46:59 +0000

    Add initial setup

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+pushd "$scriptdir" >/dev/null
+trap "popd >/dev/null" EXIT
+
+os="$(uname -s)"
+
+if [ "$os" = "Darwin" ]; then
+	nice_hostname="${HOSTNAME/%.local/}"
+	. "./macos/$nice_hostname/bootstrap.sh"
+fi
