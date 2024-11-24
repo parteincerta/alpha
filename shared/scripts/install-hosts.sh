4219da703a9d3cfd7fe2cb66fcd6932b2456fefd
Fernando Moreira <fernando@parteincerta.com>
2024-11-24 17:42:17 +0000

    Update StevenBlack's hosts to v3.14.134

--- shared/scripts/install-hosts.sh
+++ shared/scripts/install-hosts.sh
@@ -10,7 +10,7 @@ trap 'rm -rf $TMPDIR/hosts*' EXIT
 source "$rootdir/shared/scripts/helper.sh"
 trap 'trap_error;rm -rf $TMPDIR/hosts*' ERR
 
-version="3.14.133"
+version="3.14.134"
 url="https://raw.githubusercontent.com/StevenBlack/hosts/${version}/alternates/gambling-porn-social/hosts"
 system="$(uname -s)"
 
